952dac19ac9aefc8ebfbcc0c2c243d3388fb8001
DESKTOP-5TKNI0U\Zaman <mkzamanalive@gmail.com>
2017-02-04 12:19:01 +0600

    Primary Binary Search

--- Searching/Binary Search/main.cpp
+++ Searching/Binary Search/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+bool Binary_Seach(vector<T> &A, T target)
+{
+    int low = 0;
+    int high = A.size();
+    int mid;
+    while(low<=high)
+    {
+        mid = low + (high - low)/2;
+        if(A[mid] == target)
+        {
+            return true;
+        }
+        else if(A[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+
+            high = mid - 1;
+        }
+    }
+    return false;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> newVector;
+    for(int i=0; i < 100 ; i++)
+    {
+        newVector.push_back(i);
+    }
+    Binary_Seach(newVector, 56) ? cout<< "Found" : cout << "Not Found";
+    Binary_Seach(newVector, 156) ? cout<< "Found" : cout << "Not Found";
+
+
     return 0;
 }
