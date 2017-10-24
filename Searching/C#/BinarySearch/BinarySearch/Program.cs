756c2e01afdd594b3b9da0ad3c56a380ace5a128
Zaman <mkzamanalive@gmail.com>
2017-10-24 18:10:52 +0600

    Binary Search Implementation

--- Searching/C#/BinarySearch/BinarySearch/Program.cs
+++ Searching/C#/BinarySearch/BinarySearch/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace BinarySearch
 {
-    class Program
+    public class Program
     {
+        public static object BinarySearch<T>(List<T> arrayElements, T elementToFind, bool isPositionRequired)
+        {
+            int low, mid, high;
+            low = 0;
+            high = arrayElements.Count;
+            object returnObject;
+            while(low <= high)
+            {
+                mid = low + (high - low) / 2;
+                if(EqualityComparer<T>.Default.Equals(arrayElements[mid], elementToFind))
+                {
+                    returnObject = isPositionRequired ? (object)mid : true;
+                    return returnObject;
+                }
+            }
+            returnObject = isPositionRequired ? (object)-1 : false;
+            return returnObject;
+        }
+
         static void Main(string[] args)
         {
+            List<int> array = new List<int>();
+            array.Add(2);
+            array.Add(2);
+            array.Sort();
+            bool isSearchSuccess;
+            int searchPosition;
+            isSearchSuccess = (bool)BinarySearch(array, 2, false);
+            searchPosition = (int)BinarySearch(array, 2, true);
         }
     }
 }
---
+++ Searching/C#/BinarySearch/BinarySearchTest/BinarySearchTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinarySearch;
+using System.Collections.Generic;
+
+namespace BinarySearchTest
+{
+    [ ]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void IsSearchResultCorrect()
+        {
+            bool result;
+            List<int> intList = new List<int>();
+            intList.Add(1);
+            intList.Add(4);
+            intList.Add(3);
+            intList.Add(5);
+            intList.Add(2);
+            //result = (bool)Program.BinarySearch(intList, 5, false);
+            //Assert.Equals(result, true);
+            result = (bool)Program.BinarySearch(intList, 8, false);
+            Assert.AreEqual(result, true);
+        }
+    }
+}
