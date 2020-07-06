4770d8baa032d0eddbd2de27c7096a6436d37731
mkzaman <mkzamanalive@gmail.com>
2020-07-06 23:52:54 +0600

    Bubble Sort Done

---
+++ Sorting/SortThemAll/SortAlgorithms/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortThemAll.SortAlgorithms
+{
+    public class BubbleSort : ISort
+    {
+        public void Sort<T>(List<T> sortable)
+        {
+            for(int i=0; i < sortable.Count; i++)
+            {
+                for(int j=0; j < sortable.Count - 1; j++)
+                {
+                    if(Comparer<T>.Default.Compare(sortable[j], sortable[j+1]) > 0)
+                    {
+                        T temp = sortable[j];
+                        sortable[j] = sortable[j+1];
+                        sortable[j+1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- Sorting/SortingTest/UnitTest1.cs
+++ Sorting/SortingTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+
 namespace SortingTest
 {
     public class Tests
