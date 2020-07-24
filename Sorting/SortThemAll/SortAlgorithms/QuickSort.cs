bde7b5fd2a0c83b7381ab003be4245851435339c
Md. Kamruzzaman <mkzamanalive@gmail.com>
2020-07-24 18:18:40 +0600

    quicksort initial commit

---
+++ Sorting/SortThemAll/SortAlgorithms/QuickSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortThemAll.SortAlgorithms
+{
+    public class QuickSort : ISort
+    {
+        public void Sort<T>(List<T> sortable)
+        {
+            sortable = QuickSortImplementation(sortable.ToArray()).ToList();
+        }
+
+        private T[] QuickSortImplementation<T>(T[] arr)
+        {
+            if(arr.Length < 2)
+            {
+                return new T[0];
+            }
+            int pivot = 
+        }
+
+
+    }
+}
